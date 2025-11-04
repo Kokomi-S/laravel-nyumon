1d5a1d8ae69c597000f144ecaddc38960983db4d
鈴木 <suzukik@analix.co.jp>
2025-11-04 14:42:40 +0900

    問05 解答

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// 実務ではControllerを使うことが多い
+// Route::get('/station5', function () {
+//     return 'Station5';
+// }); 
+
+Route::get('/diary', function () {
+    return "Hello!";
+});
