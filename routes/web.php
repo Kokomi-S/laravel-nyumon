3b6b7003c7d3b3ce6269069ed1f635db18bf02e5
鈴木 <suzukik@analix.co.jp>
2025-11-04 15:18:17 +0900

    問06 解答

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DiaryController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-// 実務ではControllerを使うことが多い
-// Route::get('/station5', function () {
-//     return 'Station5';
-// }); 
-
-Route::get('/diary', function () {
-    return "Hello!";
-});
+Route::get('/diary', [DiaryController::class, 'index'])->name('diary.index');
